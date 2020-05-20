4ef64a87bfd8925ebc97582f9f62ca08a6945c33
lohithdhanakonda <lohith.dhanakonda@ggktech.com>
2020-05-20 21:40:31 +0530

    this works

--- paged.polyfill.js
+++ paged.polyfill.js
@@ -1410,8 +1410,8 @@
 					// console.log(rendered.getElementsByTagName('table')[rendered.getElementsByTagName('table').length-1].getElementsByTagName('tr').length,'rows count')
 					if (rendered.getElementsByTagName('table')[rendered.getElementsByTagName('table').length - 1]) {
 						let range = document.createRange();
-						range.setStart(rendered.getElementsByTagName('table')[rendered.getElementsByTagName('table').length - 1].parentElement
-							, rendered.getElementsByTagName('table')[rendered.getElementsByTagName('table').length - 1].offsetTop);
+						range.setStart(rendered.getElementsByTagName('table')[rendered.getElementsByTagName('table').length - 1]
+							, rendered.getElementsByTagName('table')[rendered.getElementsByTagName('table').length - 1].parentElement.offsetTop);
 						return range;
 					}
 				}
